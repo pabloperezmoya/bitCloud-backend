915891cd5d8e237905f52ea9e26384d6a3e76c56
Pablo <ppmoya08@gmail.com>
2023-05-31 19:54:57 -0400

    Modified app listening port

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
